ccee24a0410c409a03ae1f2cb2874c34eddb0a7e
rafscomm <rafaelpereira2033@gmail.com>
2022-11-17 15:08:12 -0300

    Inicio do Projeto

---
+++ src/components/NavBar/navBar.tsx
@@ -0,0 +1,18 @@
+
+
+function NavBar(){
+  return(
+    <nav className="nav-container">
+      <ul className="main-ul-container">
+        <li>Produtos</li>
+        <li>Clientes </li>
+        <li>Fornecedores</li>
+        <li>Venda</li>
+        <li>Caixa</li> 
+      </ul>
+    </nav>
+ )
+
+}
+
+export default NavBar;
