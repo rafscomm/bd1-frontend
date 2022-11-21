046cd5bc91787bf16cf6e84170d85b01689baeb7
rafscomm <rafaelpereira2033@gmail.com>
2022-11-21 15:07:36 -0300

    Tirando bug do login

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import { Home } from './pages/Home/home';
 import { Login } from './pages/Login';
 
 const App = (): JSX.Element => {
-  return (
+  const logged = sessionStorage.getItem('user');
+  return logged ? (
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  ) : (
+    <BrowserRouter>
+      <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Navigate to="login" />} />
       </Routes>
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,11 +1,12 @@
 import { Sidebar } from '../../components/Sidebar/Sidebar';
+import { FuncionarioInterface } from '../../interfaces/FuncionarioInterface';
 import { PageContainer } from '../../styles/PageContainer';
 
 export function Home(): JSX.Element {
   return (
     <PageContainer>
       <Sidebar />
-      <h1>Home</h1>
+      <h1>Seja Bem Vindo</h1>
     </PageContainer>
   );
 }
