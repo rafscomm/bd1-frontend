56598864af986cca2dce02a3f8f74b18e319a4bc
Rafscomm <rafaelpereira2033@gmail.com>
2022-11-22 01:27:42 -0300

    Inicio da home

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -3,10 +3,16 @@ import { FuncionarioInterface } from '../../interfaces/FuncionarioInterface';
 import { PageContainer } from '../../styles/PageContainer';
 
 export function Home(): JSX.Element {
+  const userInfo: any = sessionStorage.getItem('user');
+  const user: object = JSON.parse(userInfo);
+  console.log(user);
+  const data: Date = new Date();
+
   return (
     <PageContainer>
       <Sidebar />
-      <h1>Seja Bem Vindo</h1>
+      <h1>Seja Bem Vindo, {user.nome} </h1>
+      <h3>{data.toLocaleString()}</h3>
     </PageContainer>
   );
 }
