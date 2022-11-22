d9131e65e82f53f2fb9cbafc60c8ad28f56d24c6
Rafscomm <rafaelpereira2033@gmail.com>
2022-11-22 00:56:18 -0300

    Correcoes nas rotas

---
+++ src/pages/Clientes/indes.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from '../../components/Sidebar/Sidebar';
+import { PageContainer } from '../../styles/PageContainer';
+
+export function Clientes(): JSX.Element {
+  return (
+    <PageContainer>
+      <Sidebar />
+      <h1>Clientes</h1>
+    </PageContainer>
+  );
+}
