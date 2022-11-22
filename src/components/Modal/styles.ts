6c9c504483917821aac2ac86144283dc6a104a88
Rafscomm <rafaelpereira2033@gmail.com>
2022-11-22 20:50:00 -0300

    alterações no modal

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 
 export const ModalContainer = styled.div`
   position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.8);
   border-radius: 5px;
-
+  z-index: 1;
   top: 50%;
   left: 50%;
   right: auto;
@@ -15,16 +18,16 @@ export const ModalContainer = styled.div`
 `;
 
 export const ModalContent = styled.div`
-  position: absolute;
+  display: flex;
+  flex-direction: column;
   width: 50%;
   height: 50%;
   background-color: #fff;
   border-radius: 5px;
-
-  top: 50%;
-  left: 50%;
-  right: auto;
-  bottom: auto;
-  transform: translate(-50%, -50%);
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  .close{
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+  }
 `;
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -8,9 +8,10 @@ import { Button, ProductCardStyled } from './styles';
 
 interface ProductCardProps {
   produto: ProdutoInterface;
+  handleOpen: () => void;
 }
 
-export function ProductCard({ produto }: ProductCardProps): JSX.Element {
+export function ProductCard({ produto, handleOpen }: ProductCardProps): JSX.Element {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
@@ -23,22 +24,12 @@ export function ProductCard({ produto }: ProductCardProps): JSX.Element {
             <h1 style={{ color: '#FFC75F', fontSize: 20 }}>$</h1>
             <h3>{produto.price}</h3>
           </div>
-          <AiFillInfoCircle
-            style={{ color: '808080', marginRight: 10, fontSize: 20 }}
-            onClick={(): void => setModalIsOpen(true)}
-          />
+          <AiFillInfoCircle style={{ color: '808080', marginRight: 10, fontSize: 20 }} onClick={handleOpen} />
         </div>
         <p>{produto.nome}</p>
       </div>
       <DeleteButtom />
       <Button>Comprar</Button>
-      {modalIsOpen && (
-        <Modal onClose={(): void => setModalIsOpen(false)}>
-          <p>Nome:</p>
-          <p>Marca:</p>
-          <p>Modelo:</p>
-        </Modal>
-      )}
     </ProductCardStyled>
   );
 }
