5b6de7bba9f43a1ac828dc6a79089147ddd0a562
João Oliveira <jvcbatist4@outlook.com>
2022-11-22 23:09:30 -0300

    Fornecedores

--- src/pages/Fornecedores/index.tsx
+++ src/pages/Fornecedores/index.tsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
+import { HeaderTable } from '../../components/HeaderTable';
 import { Sidebar } from '../../components/Sidebar/Sidebar';
 import { PageContainer } from '../../styles/PageContainer';
+import { Fornecedor } from '../../interfaces/FornecedorInterface';
+import { TableContainer } from './styles';
 
 export function Fornecedores(): JSX.Element {
+  const [fornecedores, setFornecedores] = useState<Fornecedor[]>([]);
+
   return (
     <PageContainer>
       <Sidebar />
-      <h1>Fornecedores</h1>
+      <HeaderTable title="Fornecedores">
+        <TableContainer>
+          <table>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>CNPJ</th>
+              <th>Empresa</th>
+            </tr>
+            <tr>
+              {fornecedores.map((fornecedor) => (
+                <>
+                  <th>{fornecedor.idEmpresa}</th>
+                  <th>{fornecedor.nomeFantasia}</th>
+                  <th>{fornecedor.cnpj}</th>
+                  <th>{fornecedor.empresa}</th>
+                </>
+              ))}
+            </tr>
+          </table>
+        </TableContainer>
+      </HeaderTable>
     </PageContainer>
   );
 }
